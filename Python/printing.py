06ae08e9fc16eaee6e48e689381ee2893d24f04b
Zen <cyvid@pop-os.localdomain>
2020-11-09 22:35:25 +0800

    new repo

---
+++ Python/printing.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    print (*range(1, n + 1),sep = '') #the * represents an array and it is needed
+				  #to remove the brackets
+
